f2eed3e31c066fc51c10f956adf18d8af7ab63db
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-05-29 13:58:59 +0545

    Users data

---
+++ lib/features/auth/data/models/users.dart
@@ -0,0 +1,15 @@
+class User {
+  String? fullname;
+  String? email;
+  String? password;
+  String? address;
+  String? phoneNumber;
+
+  User({
+    this.fullname,
+    this.email,
+    this.password,
+    this.address,
+    this.phoneNumber,
+  });
+}
