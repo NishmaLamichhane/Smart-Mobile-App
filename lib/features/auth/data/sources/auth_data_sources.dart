6b105412986cc14c845911d9100c9644987d9d1f
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-05-30 10:38:56 +0545

    Auth data sources

---
+++ lib/features/auth/data/sources/auth_data_sources.dart
@@ -0,0 +1,27 @@
+import 'package:smart_folder_mobile_app/core/apis/api_calls.dart';
+
+abstract class AuthDataSources {
+  Future<String>registeruser({
+    required Map<String, dynamic> userData
+  }  );
+}
+  class AuthDataSourcesImpl extends AuthDataSources {
+
+final ApiCalls apiCalls;
+
+  AuthDataSourcesImpl({required this.apiCalls});
+
+  @override
+  Future<String> registeruser({
+    required Map<String, dynamic> userData,
+  }) async {
+  
+      final response = await apiCalls.postData(
+        endpoint: 'ApiConstants.registerUserRoute',
+        data: userData,
+      );
+      return 'User registered successfully: ${response['message']}';
+    } 
+  }
+
+
