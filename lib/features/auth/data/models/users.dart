c96b155e0fcac138b9ff25ca16d0222b3d69c573
NishmaLamichhane <nishmalamichhane2005@gmail.com>
2025-05-30 10:37:40 +0545

    Json Serialization

--- lib/features/auth/data/models/users.dart
+++ lib/features/auth/data/models/users.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class User {
   String? fullname;
   String? email;
@@ -12,4 +15,28 @@ class User {
     this.address,
     this.phoneNumber,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'fullname': fullname,
+      'email': email,
+      'password': password,
+      'address': address,
+      'phoneNumber': phoneNumber,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      fullname: map['fullname'] != null ? map['fullname'] as String : null,
+      email: map['email'] != null ? map['email'] as String : null,
+      password: map['password'] != null ? map['password'] as String : null,
+      address: map['address'] != null ? map['address'] as String : null,
+      phoneNumber: map['phoneNumber'] != null ? map['phoneNumber'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source) as Map<String, dynamic>);
 }
